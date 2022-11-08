0aa64a43325b65e74df26bf032bc80b2ec1723c1
Hyonoka <117749279+Hyonoka@users.noreply.github.com>
2022-11-08 15:36:35 +0100

    Add files via upload

---
+++ Kick Blocked players.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         Kick Blocked players
+// @version      1.0
+// @description  Count the number of blocked player
+// @author       hyonoka
+// @match        https://animemusicquiz.com/*
+// @grant        none
+// ==/UserScript==
+
+
+if (document.getElementById("startPage")) return;
+
+let loadInterval = setInterval(() => {
+    if (document.getElementById("loadingScreen").classList.contains("hidden")) {
+        setup();
+        clearInterval(loadInterval);
+    }
+}, 500);
+
+let blockedPlayers;
+
+function setup () {
+    blockedPlayers = socialTab.blockedPlayers;
+    new Listener("New Player", ({name}) => {
+         isOnBlockedList(name);
+    }).bindListener();
+}
+
+function isOnBlockedList(name){
+    if(blockedPlayers.includes(name)){
+        kickPlayer(name);
+    }
+}
+
+function kickPlayer(name){
+        socket.sendCommand({
+        type: 'lobby',
+        command: 'kick player',
+        data: {
+            playerName: name
+        }
+    })
+}
+
+
