68411c874d4bf8b3b3b61fe43c2aa04b4150d552
Hyonoka <117749279+Hyonoka@users.noreply.github.com>
2022-11-08 17:00:21 +0100

    Update Kick Blocked players.user.js

--- Kick Blocked players.user.js
+++ Kick Blocked players.user.js
@@ -24,6 +24,53 @@ function setup () {
     new Listener("New Player", ({name}) => {
          isOnBlockedList(name);
     }).bindListener();
+    // ==UserScript==
+// @name         Kick Blocked players
+// @version      1.0
+// @description  Count the number of blocked player
+// @author       hyonoka
+// @match        https://animemusicquiz.com/*
+// @grant        none
+// ==/UserScript==
+
+
+if (document.getElementById("startPage")) return;
+
+let loadInterval = setInterval(() => {
+    if (document.getElementById("loadingScreen").classList.contains("hidden")) {
+        setup();
+        clearInterval(loadInterval);
+    }
+}, 500);
+
+let blockedPlayers;
+
+function setup () {
+    blockedPlayers = socialTab.blockedPlayers;
+    new Listener("New Player", ({name}) => {
+         isOnBlockedList(name);
+    }).bindListener();
+    new Listener("New Spectator", ({name}) => {
+         isOnBlockedList(name);
+    }).bindListener();
+}
+
+function isOnBlockedList(name){
+    if(blockedPlayers.includes(name)){
+        kickPlayer(name);
+    }
+}
+
+function kickPlayer(name){
+        socket.sendCommand({
+        type: 'lobby',
+        command: 'kick player',
+        data: {
+            playerName: name
+        }
+    })
+}
+
 }
 
 function isOnBlockedList(name){
